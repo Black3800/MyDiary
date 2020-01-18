5c40c7eaeb3b87c7467a828d9969d6de3a7fb7fb
Arnonym <black38000@gmail.com>
2020-01-18 23:51:59 +0700

    Login and Register form

--- MyDiary/MainWindow.xaml.vb
+++ MyDiary/MainWindow.xaml.vb
@@ -1,3 +1,32 @@
 ﻿Class MainWindow
+    Dim navService As NavigationService
 
+    Private Sub Do_Login()
+        MessageBox.Show(pwd.Password, usr.Text)
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Do_Login()
+    End Sub
+
+    Private Sub Button_Click_2(sender As Object, e As RoutedEventArgs)
+        'Register Page = New Register()
+        'Dim Page As Register
+        'Page = New Register()
+        'NavigationService.Navigate(Page)
+
+        Dim f As New Register()
+        Me.Close()
+        f.Show()
+    End Sub
+
+    Private Sub Window_KeyDown(sender As Object, e As KeyEventArgs)
+        If e.Key = 6 Then
+            Do_Login()
+        End If
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        navService = NavigationService.GetNavigationService(Me)
+    End Sub
 End Class
---
+++ MyDiary/Register.xaml.vb
@@ -0,0 +1,18 @@
+﻿Public Class Register
+    Private Sub Do_Register()
+        MessageBox.Show(usr.Text & " " & pwd.Password, "Register")
+        Dim w As New MainWindow()
+        w.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Do_Register()
+    End Sub
+
+    Private Sub Window_KeyDown(sender As Object, e As KeyEventArgs)
+        If e.Key = 6 Then
+            Do_Register()
+        End If
+    End Sub
+End Class
